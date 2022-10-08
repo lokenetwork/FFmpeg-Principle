4965883a1bd51eb415783e7d9cb4d64c2bfbab26
Loken <loken2022@qq.com>
2022-10-08 19:13:41 +0800

    add

--- aformat_filter/main.c
+++ aformat_filter/main.c
@@ -6,8 +6,6 @@
 #include "libavutil/bprint.h"
 #include "libavutil/pixdesc.h"
 
-int save_yuv_to_file(AVFrame *frame, int num);
-
 int main()
 {
 
@@ -25,9 +23,9 @@ int main()
         return err;
     }
 
-    //打开解码器
+    //打开音频解码器
     AVCodecContext *avctx = avcodec_alloc_context3(NULL);
-    ret = avcodec_parameters_to_context(avctx, fmt_ctx->streams[0]->codecpar);
+    ret = avcodec_parameters_to_context(avctx, fmt_ctx->streams[1]->codecpar);
     if (ret < 0){
         printf("error code %d \n",ret);
         return ret;
@@ -46,8 +44,10 @@ int main()
     AVFrame *frame = av_frame_alloc();
     AVFrame *result_frame = av_frame_alloc();
 
-    const AVPixFmtDescriptor *origin_fmt = NULL;
-    const AVPixFmtDescriptor *result_fmt = NULL;
+
+    const char *origin_fmt = NULL;
+    const char *result_fmt = NULL;
+
 
     int read_end = 0;
     int frame_num = 0;
@@ -57,8 +57,8 @@ int main()
         }
 
         ret = av_read_frame(fmt_ctx, pkt);
-        //跳过不处理音频包
-        if( 1 == pkt->stream_index ){
+        //跳过不处理视频包
+        if( 0 == pkt->stream_index ){
             av_packet_unref(pkt);
             continue;
         }
@@ -101,7 +101,6 @@ int main()
                 /* 提示 AVERROR_EOF 代表之前已经往 解码器发送了一个 data 跟 size 都是 NULL 的 AVPacket
                  * 发送 NULL 的 AVPacket 是提示解码器把所有的缓存帧全都刷出来。
                  * 通常只有在 读完输入文件才会发送 NULL 的 AVPacket，或者需要用现有的解码器解码另一个的视频流才会这么干。
-                 *
                  * */
 
                 //跳出 第二层 for，文件已经解码完毕。
@@ -121,19 +120,18 @@ int main()
 
                     // 因为 filter 的输入是 AVFrame ，所以 filter 的时间基就是 AVFrame 的时间基
                     AVRational tb = fmt_ctx->streams[0]->time_base;
-                    AVRational fr = av_guess_frame_rate(fmt_ctx, fmt_ctx->streams[0], NULL);
-                    AVRational sar = frame->sample_aspect_ratio;
 
-                    origin_fmt = av_pix_fmt_desc_get(frame->format);
-                    printf("origin frame fmt is %d,%s \n",frame->format,origin_fmt->name);
+                    origin_fmt = av_get_sample_fmt_name(frame->format);
+                    printf("origin frame fmt is %d,%s \n",frame->format,origin_fmt);
 
                     AVBPrint args;
                     av_bprint_init(&args, 0, AV_BPRINT_SIZE_AUTOMATIC);
                     av_bprintf(&args,
-                                "buffer=video_size=%dx%d:pix_fmt=%d:time_base=%d/%d:pixel_aspect=%d/%d:frame_rate=%d/%d[main];"
-                                "[main]format=pix_fmts=yuyv422[result];"
+                                "abuffer=sample_rate=%d:sample_fmt=%s:channel_layout=%d:time_base=%d/%d[main];"
+                                "[main]aformat=sample_rates=%d:sample_fmts=%s:channel_layouts=%d[result];"
                                 "[result]buffersink",
-                               frame->width, frame->height, frame->format, tb.num,tb.den,sar.num, sar.den,fr.num, fr.den);
+                               frame->sample_rate, origin_fmt, (int)frame->channel_layout, tb.num,tb.den,
+                               48000,"s64",AV_CH_FRONT_RIGHT);
 
                     //解析滤镜字符串。
                     ret = avfilter_graph_parse2(filter_graph, args.str, &inputs, &outputs);
@@ -162,17 +160,13 @@ int main()
 
                 ret = av_buffersink_get_frame_flags(resultsink_ctx, result_frame,AV_BUFFERSRC_FLAG_PUSH);
                 if( ret >= 0 ){
-                    result_fmt = av_pix_fmt_desc_get(result_frame->format);
-                    printf("result frame fmt is %d,%s \n",result_frame->format,result_fmt->name);
-                    //保存进去文件。
-                    printf("save_yuv_to_file success\n");
-                    save_yuv_to_file(result_frame,frame_num);
-                    av_frame_unref(result_frame);
-                    av_frame_unref(frame);
+                    result_fmt = av_get_sample_fmt_name(result_frame->format);
+                    printf("result frame fmt is %d,%s \n",result_frame->format,result_fmt);
+
                 }
 
                 frame_num++;
-                //只保存 10 张图片
+                //只处理 10 帧音频
                 if( frame_num > 10 ){
                     return 666;
                 }
@@ -200,20 +194,5 @@ int main()
     //释放滤镜。
     avfilter_graph_free(&filter_graph);
 
-    return 0;
-}
-
-int save_yuv_to_file(AVFrame *frame, int num){
-    //拼接文件名
-    char yuv_pic_name[200] = {0};
-    sprintf(yuv_pic_name,"./yuv420p_%d.yuv",num);
-
-    //写入文件
-    FILE *fp = NULL;
-    fp = fopen(yuv_pic_name, "wb+");
-    fwrite(frame->data[0] , 1, frame->linesize[0] * frame->height, fp);
-    fwrite(frame->data[1] , 1, frame->linesize[1] * frame->height/2, fp);
-    fwrite(frame->data[2] , 1, frame->linesize[2] * frame->height/2, fp);
-    fclose(fp);
     return 0;
 }
